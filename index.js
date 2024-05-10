1cbc2c5435927104417c3701c77e9818e25a74db
Chaitanya Munje <37774110+ChaitanyaMunje@users.noreply.github.com>
2024-05-10 17:00:35 +0530

    Commitented Changes

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ app.use('/uploads', express.static(join(__dirname, 'uploads')));
 
 app.use(express.static(path.join(__dirname,"frontEnd/dist")))
 
-app.get("*", (req,res)=>{
-  res.sendFile(path.resolve(__dirname, "frontEnd", "dist", "index.html"));
-})
+// app.get("*", (req,res)=>{
+//   res.sendFile(path.resolve(__dirname, "frontEnd", "dist", "index.html"));
+// })
 
 
 
