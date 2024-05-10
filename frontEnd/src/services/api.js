2a565edc14d7e8dc1d3b31916c5e24d0257d4d48
Chaitanya Munje <37774110+ChaitanyaMunje@users.noreply.github.com>
2024-05-10 17:21:51 +0530

    Added backend Url to Frontend

--- frontEnd/src/services/api.js
+++ frontEnd/src/services/api.js
@@ -1,24 +1,24 @@
 
 // export const BASE_URL = "http://localhost:9090/"
-export const SIGNUP_API = `${window.location.origin}/api/v1/auth/register`
+export const SIGNUP_API = `https://mern-blog-app-pl56.vercel.app/api/v1/auth/register`
 
-export const LOGIN_API = `${window.location.origin}/api/v1/auth/login`
+export const LOGIN_API = `https://mern-blog-app-pl56.vercel.app/api/v1/auth/login`
 
-export const GET_ALL_CATEGORY_API = `${window.location.origin}/api/v1/category/`
+export const GET_ALL_CATEGORY_API = `https://mern-blog-app-pl56.vercel.app/api/v1/category/`
 
-export const GET_ALL_POST_API = `${window.location.origin}/api/v1/posts/`
+export const GET_ALL_POST_API = `https://mern-blog-app-pl56.vercel.app/api/v1/posts/`
 
-export const GET_POST_BY_ID_API = `${window.location.origin}/api/v1/posts/`
+export const GET_POST_BY_ID_API = `https://mern-blog-app-pl56.vercel.app/api/v1/posts/`
 
 // export const ADD_COMMENT_API = `http://localhost:9090/api/v1/posts/${postId}/comments`
 
-export const GET_POST_BY_CATEGORY_API = `${window.location.origin}/api/v1/posts/category/:category`
+export const GET_POST_BY_CATEGORY_API = `https://mern-blog-app-pl56.vercel.app/api/v1/posts/category/:category`
 
-export const GET_POST_BY_USER = `${window.location.origin}/api/v1/posts/user`
+export const GET_POST_BY_USER = `https://mern-blog-app-pl56.vercel.app/api/v1/posts/user`
 
-export const ADD_POST_API = `${window.location.origin}/api/v1/posts/addPost`;
+export const ADD_POST_API = `https://mern-blog-app-pl56.vercel.app/api/v1/posts/addPost`;
 
-export const GET_ALL_COMMENTS_API = `${window.location.origin}/api/v1/comments/post/`;
+export const GET_ALL_COMMENTS_API = `https://mern-blog-app-pl56.vercel.app/api/v1/comments/post/`;
 
-export const ADD_COMMENT_BY_POST_ID = `${window.location.origin}/api/v1/comments/post/`;
+export const ADD_COMMENT_BY_POST_ID = `https://mern-blog-app-pl56.vercel.app/api/v1/comments/post/`;
 
