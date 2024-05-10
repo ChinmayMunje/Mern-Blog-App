6c2725bae3575ee1106eeddd02ed22b90c5b9574
Chaitanya Munje <37774110+ChaitanyaMunje@users.noreply.github.com>
2024-05-10 16:58:19 +0530

    Added Resolve

--- index.js
+++ index.js
@@ -22,7 +22,13 @@ const app = express();
 // app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/uploads', express.static(join(__dirname, 'uploads')));
 
-app.use(express.static(path.join(__dirname,"public")))
+app.use(express.static(path.join(__dirname,"frontEnd/dist")))
+
+app.get("*", (req,res)=>{
+  res.sendFile(path.resolve(__dirname, "frontEnd", "dist", "index.html"));
+})
+
+
 
 
 const port = process.env.PORT || 8000
